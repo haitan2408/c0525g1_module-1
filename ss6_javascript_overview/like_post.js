05f2c1547b082e4aea584f5bb1ff2dc8e0d752b2
haitan2408 <haitan28102408@gmail.com>
2025-06-09 11:16:13 +0700

    up

---
+++ ss6_javascript_overview/like_post.js
@@ -0,0 +1,5 @@
+
+document.getElementById("btn").addEventListener("click", function () {
+    document.getElementById("btn").style.backgroundColor="blue";
+    document.getElementById("btn").style.color="white";
+})
